d55a5d241ea697eceb6e4525dba4cabb98162c4a
sujan pal <sujanpal79@gmail.com>
2024-10-25 00:05:45 +0530

    Register TNDB API & Create an app & get access token & get data from TMDB

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,11 @@
 export const LOGO = 'https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png';
 
 export const USER_AVATAR = 'https://occ-0-1492-3663.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229';
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYzM0Yjk0N2NjZDM3ZDFhMjkzYTJjMzMzNDkzZTJlOSIsIm5iZiI6MTcyOTc4OTM5OS4xNTAyNjcsInN1YiI6IjY3MWE3NzJhNzY5MTA3ZDc3YjQ3OTgyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tAoFkvpWCt347YP39mLcMuzk_N1A7gmY6I80CER2UGs'
+    }
+  };
